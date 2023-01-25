8db253461d1780696fc8e06f48a9dc07eab668bd
jaikrishnaverma-cedcoss <jaikrishnaverma@cedcoss.com>
2023-01-25 15:04:46 +0530

    commited 1

---
+++ config/database.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+exports.connect = () => {
+  // Connecting to the database
+  mongoose
+    .connect("mongodb://localhost:27017", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then(() => {
+      console.log("Successfully connected to database");
+    })
+    .catch((error) => {
+      console.log("database connection failed. exiting now...");
+      console.error(error);
+      process.exit(1);
+    });
+};
